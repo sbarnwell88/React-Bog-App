64b6e1233c92767f64911aa8f6d20a6afcdf768d
sbarnwell88 <susy6688@gmail.com>
2017-09-07 10:56:50 -0400

    created models and seeds

---
+++ app/models/creature.rb
@@ -0,0 +1,3 @@
+class Creature < ApplicationRecord
+    has_many :pets
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
+Creature.destroy_all
+
+luke = Creature.create({name: "Luke", description: "Jedi"})
+darth = Creature.create({name: "Darth Vader", description: "Father of Luke"})
+
+doggy = Pet.create({name: "doggy" })
+tarantula = Pet.create({name: "tarantula" })
+
+luke.pets = [
+    doggy
+]
+
+darth.pets = [
+    tarantula
+]
+
+
+
+
